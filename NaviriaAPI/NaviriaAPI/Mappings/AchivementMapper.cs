d4e5b0192a1c83275b7c99dbb56637aec0637eb8
anesiia <anastasiia.chervenko@gmail.com>
2025-02-22 20:08:49 +0200

    Achivement description bug fixed

--- NaviriaAPI/NaviriaAPI/Mappings/AchivementMapper.cs
+++ NaviriaAPI/NaviriaAPI/Mappings/AchivementMapper.cs
@@ -3,6 +3,7 @@ using NaviriaAPI.DTOs.UpdateDTOs;
 using NaviriaAPI.DTOs;
 using NaviriaAPI.Entities;
 using NaviriaAPI.DTOs.UdateDTOs;
+using SharpCompress.Common;
 
 namespace NaviriaAPI.Mappings
 {
@@ -10,18 +11,18 @@ namespace NaviriaAPI.Mappings
     {
         // Entity → DTO (для відправки в API)
         public static AchivementDto ToDto(AchivementEntity entity) =>
-            new AchivementDto { Id = entity.Id, Name = entity.Name };
+            new AchivementDto { Id = entity.Id, Name = entity.Name, Description = entity.Description };
 
         // 🟩 DTO → Entity (для збереження в БД)
         public static AchivementEntity ToEntity(AchivementDto dto) =>
-            new AchivementEntity { Id = dto.Id, Name = dto.Name };
+            new AchivementEntity { Id = dto.Id, Name = dto.Name, Description = dto.Description };
 
         // CreateDto → Entity (для створення)
         public static AchivementEntity ToEntity(AchivementCreateDto dto) =>
-            new AchivementEntity { Name = dto.Name };
+            new AchivementEntity { Name = dto.Name, Description = dto.Description };
 
         // UpdateDto → Entity (для оновлення)
         public static AchivementEntity ToEntity(string id, AchivementUpdateDto dto) =>
-            new AchivementEntity { Id = id, Name = dto.Name };
+            new AchivementEntity { Id = id, Name = dto.Name, Description = dto.Description };
     }
 }
