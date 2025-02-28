732060572693700ae3de7b9ff88a7eda62216749
anesiia <anastasiia.chervenko@gmail.com>
2025-02-28 15:11:56 +0200

    added logging in CategoryController and fixed endpoints

--- NaviriaAPI/NaviriaAPI/Controllers/CategoryController.cs
+++ NaviriaAPI/NaviriaAPI/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using NaviriaAPI.DTOs.CreateDTOs;
 using NaviriaAPI.DTOs.UpdateDTOs;
 using NaviriaAPI.Services;
 using NaviriaAPI.IServices;
+using NaviriaAPI.DTOs;
 
 namespace NaviriaAPI.Controllers
 {
@@ -11,46 +12,93 @@ namespace NaviriaAPI.Controllers
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
+        private readonly ILogger<AchievementsController> _logger;
 
-        public CategoryController(ICategoryService categoryService)
+        public CategoryController(ICategoryService categoryService, ILogger<AchievementsController> logger)
         {
             _categoryService = categoryService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var categories = await _categoryService.GetAllAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = await _categoryService.GetAllAsync();
+                return Ok(categories);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get categories");
+                return StatusCode(500, "Failed to get categories");
+            }
+
+            
         }
         
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
-            var category = await _categoryService.GetByIdAsync(id);
-            if (category == null) return NotFound();
-            return Ok(category);
+            try
+            {
+                var category = await _categoryService.GetByIdAsync(id);
+                if (category == null) return NotFound();
+                return Ok(category);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get category with ID {0}", id);
+                return StatusCode(500, $"Failed to get category with ID {id}");
+            }
+            
         }
 
-        [HttpPost("add")]
+        [HttpPost]
         public async Task<IActionResult> Create([FromBody] CategoryCreateDto categoryDto)
         {
-            var createdCategory = await _categoryService.CreateAsync(categoryDto);
-            return CreatedAtAction(nameof(GetById), new { id = createdCategory.Id }, createdCategory);
+            try
+            {
+                var createdCategory = await _categoryService.CreateAsync(categoryDto);
+                return CreatedAtAction(nameof(GetById), new { id = createdCategory.Id }, createdCategory);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to add new category");
+                return StatusCode(500, "Failed to add new category");
+            }
+            
         }
 
-        [HttpPut("update/{id}")]
+        [HttpPut("{id}")]
         public async Task<IActionResult> Update(string id, [FromBody] CategoryUpdateDto categoryDto)
         {
-            var updated = await _categoryService.UpdateAsync(id, categoryDto);
-            return updated ? NoContent() : NotFound();
+            try
+            {
+                var updated = await _categoryService.UpdateAsync(id, categoryDto);
+                return updated ? NoContent() : NotFound();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update category with ID {0}", id);
+                return StatusCode(500, $"Failed to update category with ID {id}");
+            }
+            
         }
 
-        [HttpDelete("delete/{id}")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string id)
         {
-            var deleted = await _categoryService.DeleteAsync(id);
-            return deleted ? NoContent() : NotFound();
+            try
+            {
+                var deleted = await _categoryService.DeleteAsync(id);
+                return deleted ? NoContent() : NotFound(); ;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to delete category with ID {0}", id);
+                return StatusCode(500, $"Failed to delete category with ID {id}");
+            }
         }
     }
 }
