3776fc7e30852f0477a437678f47f9a7053ec171
anesiia <anastasiia.chervenko@gmail.com>
2025-02-28 14:51:52 +0200

    added logging for Achievements and fixed CRUD enpoints

--- NaviriaAPI/NaviriaAPI/Controllers/AchivementsController.cs
+++ NaviriaAPI/NaviriaAPI/Controllers/AchivementsController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NaviriaAPI.DTOs.CreateDTOs;
-using NaviriaAPI.DTOs.UpdateDTOs;
-using NaviriaAPI.Services;
 using NaviriaAPI.IServices;
-using NaviriaAPI.DTOs;
 using NaviriaAPI.DTOs.UdateDTOs;
 
 namespace NaviriaAPI.Controllers
@@ -13,46 +10,88 @@ namespace NaviriaAPI.Controllers
     public class AchivementsController : ControllerBase
     {
         private readonly IAchivementService _achivementsService;
+        private readonly ILogger<AchivementsController> _logger;
 
-        public AchivementsController(IAchivementService achivementsService)
+        public AchivementsController(IAchivementService achivementsService, ILogger<AchivementsController> logger)
         {
             _achivementsService = achivementsService;
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var achivements = await _achivementsService.GetAllAsync();
-            return Ok(achivements);
+            try
+            {
+                var achivements = await _achivementsService.GetAllAsync();
+                return Ok(achivements);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get achivements");
+                return StatusCode(500, "Failed to get achivements");
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(string id)
         {
-            var achivement = await _achivementsService.GetByIdAsync(id);
-            if (achivement == null) return NotFound();
-            return Ok(achivement);
+            try
+            {
+                var achivement = await _achivementsService.GetByIdAsync(id);
+                if (achivement == null) return NotFound();
+                return Ok(achivement);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get achivement with ID {0}", id);
+                return StatusCode(500, $"Failed to get achivement with ID {id}");
+            }
         }
 
-        [HttpPost("add")]
+        [HttpPost]
         public async Task<IActionResult> Create([FromBody] AchivementCreateDto achivementDto)
         {
-            var createdAchivement = await _achivementsService.CreateAsync(achivementDto);
-            return CreatedAtAction(nameof(GetById), new { id = createdAchivement.Id }, createdAchivement);
+            try
+            {
+                var createdAchivement = await _achivementsService.CreateAsync(achivementDto);
+                return CreatedAtAction(nameof(GetById), new { id = createdAchivement.Id }, createdAchivement);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to add new achivement");
+                return StatusCode(500, "Failed to add new achivement");
+            }
         }
 
-        [HttpPut("update/{id}")]
+        [HttpPut("{id}")]
         public async Task<IActionResult> Update(string id, [FromBody] AchivementUpdateDto achivementDto)
         {
-            var updated = await _achivementsService.UpdateAsync(id, achivementDto);
-            return updated ? NoContent() : NotFound();
+            try
+            {
+                var updated = await _achivementsService.UpdateAsync(id, achivementDto);
+                return updated ? NoContent() : NotFound();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update achivement with ID {0}", id);
+                return StatusCode(500, $"Failed to update achivement with ID {id}");
+            }
         }
 
-        [HttpDelete("delete/{id}")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(string id)
         {
-            var deleted = await _achivementsService.DeleteAsync(id);
-            return deleted ? NoContent() : NotFound();
+            try
+            {
+                var deleted = await _achivementsService.DeleteAsync(id);
+                return deleted ? NoContent() : NotFound();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to delete achivement with ID {0}", id);
+                return StatusCode(500, $"Failed to delete achivement with ID {id}");
+            }
         }
     }
 }
