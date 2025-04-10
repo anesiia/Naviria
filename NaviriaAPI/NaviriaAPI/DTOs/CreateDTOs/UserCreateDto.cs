8aa6508b9972cf0b3f999f1d9ecc43c46836cc78
anesiia <anastasiia.chervenko@gmail.com>
2025-04-10 19:33:38 +0300

    updated email validation

--- NaviriaAPI/NaviriaAPI/DTOs/CreateDTOs/UserCreateDto.cs
+++ NaviriaAPI/NaviriaAPI/DTOs/CreateDTOs/UserCreateDto.cs
@@ -28,6 +28,7 @@ namespace NaviriaAPI.DTOs.CreateDTOs
 
         [Required]
         [EmailAddress]
+        [RegularExpression("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
         public string Email { get; set; } = string.Empty;
 
         [Required]
