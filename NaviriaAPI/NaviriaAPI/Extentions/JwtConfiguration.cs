4d44dd7c4665774b8f6f4e629f1df386e330e884
anesiia <anastasiia.chervenko@gmail.com>
2025-04-20 23:01:34 +0300

    added token to presence hub

--- NaviriaAPI/NaviriaAPI/Extentions/JwtConfiguration.cs
+++ NaviriaAPI/NaviriaAPI/Extentions/JwtConfiguration.cs
@@ -29,6 +29,22 @@ namespace NaviriaAPI.Extentions
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!)),
                     ClockSkew = TimeSpan.Zero
                 };
+
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs/presence"))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
             });
         }
     }
