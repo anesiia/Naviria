70e4b0cf726c3299dbb0344408d1b17c2f2bc83a
anesiia <anastasiia.chervenko@gmail.com>
2025-04-22 14:28:59 +0300

    align method parameter names with interface declaration

--- NaviriaAPI/NaviriaAPI/IServices/IAchievementService.cs
+++ NaviriaAPI/NaviriaAPI/IServices/IAchievementService.cs
@@ -8,8 +8,8 @@ namespace NaviriaAPI.IServices
     {
         Task<IEnumerable<AchievementDto>> GetAllAsync();
         Task<AchievementDto?> GetByIdAsync(string id);
-        Task<AchievementDto> CreateAsync(AchievementCreateDto AchievementDto);
-        Task<bool> UpdateAsync(string id, AchievementUpdateDto  AchievementDto);
+        Task<AchievementDto> CreateAsync(AchievementCreateDto achievementDto);
+        Task<bool> UpdateAsync(string id, AchievementUpdateDto  achievementDto);
         Task<bool> DeleteAsync(string id);
     }
 }
