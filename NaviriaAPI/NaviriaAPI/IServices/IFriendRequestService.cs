21af437c3b4661004521c8414d44e02cb59ed27c
anesiia <anastasiia.chervenko@gmail.com>
2025-04-23 17:39:38 +0300

    code enhanced

--- NaviriaAPI/NaviriaAPI/IServices/IFriendRequestService.cs
+++ NaviriaAPI/NaviriaAPI/IServices/IFriendRequestService.cs
@@ -3,13 +3,17 @@ using NaviriaAPI.DTOs.UpdateDTOs;
 using NaviriaAPI.DTOs;
 using System;
 
-public interface IFriendRequestService
+namespace NaviriaAPI.IServices
 {
-    Task<IEnumerable<FriendRequestDto>> GetAllAsync();
-    Task<FriendRequestDto?> GetByIdAsync(string id);
-    Task<FriendRequestDto> CreateAsync(FriendRequestCreateDto friendRequestDto);
-    Task<bool> UpdateAsync(string id, FriendRequestUpdateDto friendRequestDto);
-    Task<bool> DeleteAsync(string id);
-    Task<IEnumerable<FriendRequestDto>> GetIncomingRequestsAsync(string toUserId);
+    public interface IFriendRequestService
+    {
+        Task<IEnumerable<FriendRequestDto>> GetAllAsync();
+        Task<FriendRequestDto?> GetByIdAsync(string id);
+        Task<FriendRequestDto> CreateAsync(FriendRequestCreateDto friendRequestDto);
+        Task<bool> UpdateAsync(string id, FriendRequestUpdateDto friendRequestDto);
+        Task<bool> DeleteAsync(string id);
+        Task<IEnumerable<FriendRequestDto>> GetIncomingRequestsAsync(string toUserId);
 
+    }
 }
+
