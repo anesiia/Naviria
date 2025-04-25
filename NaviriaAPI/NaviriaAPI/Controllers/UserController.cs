3d53c3a9cc119a6b99b983c98ed1fe00f05bfdb0
anesiia <anastasiia.chervenko@gmail.com>
2025-04-25 13:28:08 +0300

    work with friends relocated from user controller to friends controller

--- NaviriaAPI/NaviriaAPI/Controllers/UserController.cs
+++ NaviriaAPI/NaviriaAPI/Controllers/UserController.cs
@@ -1,14 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NaviriaAPI.DTOs.CreateDTOs;
 using NaviriaAPI.DTOs.UpdateDTOs;
 using NaviriaAPI.IServices;
 using NaviriaAPI.IServices.ICloudStorage;
-using NaviriaAPI.Entities;
-using NaviriaAPI.DTOs;
 using Microsoft.AspNetCore.Authorization;
-using NaviriaAPI.Services.CloudStorage;
-using Newtonsoft.Json.Linq;
 
 namespace NaviriaAPI.Controllers
 {
@@ -18,20 +13,17 @@ namespace NaviriaAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
-        private readonly IFriendService _friendService;
         private readonly ILogger<UserController> _logger;
         public readonly ICloudinaryService _cloudinaryService;
 
         public UserController(
             IUserService userService,
             ILogger<UserController> logger,
-            ICloudinaryService cloudinaryService,
-            IFriendService friendService)
+            ICloudinaryService cloudinaryService)
         {
             _userService = userService;
             _logger = logger;
             _cloudinaryService = cloudinaryService;
-            _friendService = friendService;
         }
 
         [AllowAnonymous]
@@ -129,77 +121,5 @@ namespace NaviriaAPI.Controllers
                 return StatusCode(500, $"Failed to delete user with ID {id}");
             }
         }
-
-        [HttpGet("{id}/friends")]
-        public async Task<IActionResult> GetFriends(string id)
-        {
-            if (string.IsNullOrWhiteSpace(id))
-                return BadRequest("User ID is required.");
-
-            try
-            {
-                var friends = await _friendService.GetUserFriendsAsync(id);
-                return Ok(friends);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to get friends for user {Id}", id);
-                return StatusCode(500, $"Failed to get friends for user {id}");
-            }
-        }
-
-        [HttpDelete("{fromUserId}/friends/{friendId}")]
-        public async Task<IActionResult> DeleteFriend(string fromUserId, string friendId)
-        {
-            if (string.IsNullOrWhiteSpace(fromUserId))
-                return BadRequest("User ID is required.");
-            if (string.IsNullOrWhiteSpace(friendId))
-                return BadRequest("Friend ID is required.");
-
-            try
-            {
-                var deleted = await _friendService.DeleteFriendAsync(fromUserId, friendId);
-                return deleted ? NoContent() : NotFound();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to delete friend of user with ID {0}", fromUserId);
-                return StatusCode(500, $"Failed to delete friend of user with ID {fromUserId}");
-            }
-        }
-
-        [HttpGet("{id}/potential-friends")]
-        public async Task<IActionResult> GetPotentialFriends(string id)
-        {
-            try
-            {
-                var users = await _friendService.GetPotentialFriendsAsync(id);
-                return Ok(users);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to get potential friends for user {UserId}", id);
-                return StatusCode(500, "An error occurred while retrieving potential friends.");
-            }
-        }
-
-        [HttpGet("{userId}/search-friends")]
-        public async Task<IActionResult> SearchFriendsByNickname(string userId, [FromQuery] string query)
-        {
-            if (string.IsNullOrWhiteSpace(query))
-                return BadRequest("Search query is required.");
-
-            try
-            {
-                var results = await _friendService.SearchUsersByNicknameAsync(userId, query);
-                return Ok(results);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to search users by nickname for user {UserId}", userId);
-                return StatusCode(500, "Error searching for users.");
-            }
-        }
-
     }
 }
