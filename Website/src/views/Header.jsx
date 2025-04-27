e7ea34ac2d7721b0fc1c2b66e4b193b28d4715dd
Nadiia Dashko <nadiia.dashko@nure.ua>
2025-04-27 21:26:51 +0300

    added nitifications

--- Website/src/views/Header.jsx
+++ Website/src/views/Header.jsx
@@ -1,8 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Notifications from "./Notifications";
+import { useState, useEffect, useRef } from "react";
+import {
+  getNotifications,
+  markAllNotificationsRead,
+} from "../services/NotificationsServices";
+
 import "../styles/header.css";
 
 export default function Header() {
+  const [showNotifications, setShowNotifications] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+  const ref = useRef(null);
+
+  const handleToggleNotifications = async () => {
+    if (!showNotifications) {
+      try {
+        const data = await getNotifications();
+        setNotifications(data);
+        await markAllNotificationsRead();
+      } catch (err) {
+        console.error("Помилка завантаження повідомлень:", err.message);
+      }
+    }
+    setShowNotifications(!showNotifications);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setShowNotifications(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className="site-header">
       <img className="logo" src="logo.svg" />
@@ -14,9 +50,10 @@ export default function Header() {
         <Link to="/friends">Ком'юніті</Link>
       </nav>
       <div className="actions">
-        <button className="notifications">
+        <button className="notifications" onClick={handleToggleNotifications}>
           <img src="bell.svg" alt="notifications" />
         </button>
+        {showNotifications && <Notifications notifications={notifications} />}
         <Link to="/profile">
           <img className="avatar" src="Ellipse.svg" alt="avatar" />
         </Link>
