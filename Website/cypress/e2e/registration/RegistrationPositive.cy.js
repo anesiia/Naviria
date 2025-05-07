ad6ad35879cededf55120948ddf2ee0d8398c1b4
Mariam Almakadma <mariam.almakadma@nure.ua>
2025-05-07 10:36:20 +0200

    update

--- Website/cypress/e2e/registration/RegistrationPositive.cy.js
+++ Website/cypress/e2e/registration/RegistrationPositive.cy.js
@@ -10,9 +10,10 @@ describe('Registration Page', () => {
     cy.get('input[name="name"]').type('Марія');
     cy.get('input[name="surname"]').type('Тестова');
     cy.get('input[name="email"]').type(`testuser${Date.now()}@example.com`); // Унікальна пошта
+
     cy.get('input[name="password"]').type('StrongPass123');
     cy.get('input[name="repeat-password"]').type('StrongPass123');
-    cy.get('input[name="nickname"]').type('TestUser3000');
+    cy.get('input[name="nickname"]').type(`TestUser3000${Date.now()}p`);
     cy.get('input[name="birth-date"]').type('2000-01-01');
     cy.get('input[name="gender"][value="F"]').check();
 
