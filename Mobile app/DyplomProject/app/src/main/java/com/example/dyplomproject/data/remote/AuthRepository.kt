6b45fc9a0562fbdf66651f10de24a641a907659b
eliza <eliza@MOJ_KOMPUTER>
2025-05-21 22:19:07 +0200

    there were added different error messages depending on the code of response from the server on Registration page

--- Mobile app/DyplomProject/app/src/main/java/com/example/dyplomproject/data/remote/AuthRepository.kt
+++ Mobile app/DyplomProject/app/src/main/java/com/example/dyplomproject/data/remote/AuthRepository.kt
@@ -52,6 +52,8 @@ class AuthRepository(private val apiService: ApiService) {
 //                )
 //                Result.failure(Exception("Registration failed with code ${response.code()}: $errorBody"))
                 val userMessage = when (response.code()) {
+                    409 -> "Пошта вже використовується! Ввійдіть в вже створений акаунт або використайте іншу пошту для реєстрації"
+                    422 -> "Нікнейм зайнятий, вигадайте інший!"
                     500 -> "Пошта або нікнейм вже використовуються! Використовуйте іншу пошту або змініть нік"
                     else -> "Невідома помилка: ${response.code()}"
                 }
